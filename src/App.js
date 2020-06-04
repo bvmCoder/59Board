15c392f5d489fbd6c7acb1157527b53bb17bca36
@bvmCoder <talkwithdixit@gmail.com>
2020-06-04 04:45:51 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import './App.css';
+
+import Home from './routes/Home'
+
+
+function App() {
+  return (
+    <Router>
+        <Switch>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </Router>
+  );
+}
+
+export default App;
